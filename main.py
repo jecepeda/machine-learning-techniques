0ebf17f385b9b7147ef24a6fba9bf47f8cc94c0d
TheRaez <pabloraez.1995@gmail.com>
2016-10-11 19:23:20 +0200

    Esto funciona que te cagas, toquemos el ukelele <3

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-from .user import Incidencia, get_db_session
+from os import path, remove
+from datetime import datetime
+from models import Incidencia, get_db_session, create_db
 
+#Gets an incidence and divides it in attributes, then it stores it into it's attribute into the result array
+#returns the result array
 def clean_incidence(raw_incidence):
     result = {}
     raw_attributes = [item for item in raw_incidence.split("\n") if item != '']
@@ -11,18 +15,25 @@ def clean_incidence(raw_incidence):
 
     return result
 
+#return the key from an attribute
 def extract_key(attribute):
     return attribute[1:attribute.index(">")]
 
+#Removes the key from the attribute
+#returns the value from the key field given
 def get_value(key, attribute):
     words_to_remove = ["<{}>".format(key),"</{}>".format(key)]
     return replace_and_clean_data(words_to_remove, attribute)
 
+#Removes every selected word in list "words" from "data"
+#return the same data without those words
 def replace_and_clean_data(words, data):
     for item in words:
         data = data.replace(item, "")
     return data
 
+#Opens the file and removes the words "<raiz>, </raiz>, </incidenciaGeolocalizada> & \t"
+#return the file into a vector, separated in each incidenciaGeolocalizada
 def load_and_clean_data(path):
     data = open(path, encoding="ISO-8859-1")
 
@@ -32,19 +43,57 @@ def load_and_clean_data(path):
 
     return data_cleaned
 
+#Get the data already cleaned and sorted in an array and cleans all it's incidences
+#returns the concatenation of those dictionaries
 def extract_data(data):
     result = []
-    
+
     for raw_incidence in data:
         incidence_dict = clean_incidence(raw_incidence)
         result.append(incidence_dict)
-        
+
     return result
 
-def insert_incidences_into_db(incidences):
-    
+#Gets the incidences and turns them into db objects
+#it also inputs those into the db
+def insert_incidences_into_db(incidences, db):
+
+    for incidence in incidences:
+        incidence_object = Incidencia()
+        for key in incidence.keys():
+            if incidence[key] is not '':
+                incidence_object.__setattr__(key, turn_to_datatype(key, incidence[key]))
+        db.add(incidence_object)
+    db.commit()
+
+#Convierte una string a float, int, DateTime, o lo mantiene en string dependiendo del attrib
+#return el valor convertido o no.
+def turn_to_datatype(attrib, value):
+    float_attrib = ["longitud", "latitud", "pk_inicial", "pk_final"]
+
+    if attrib in float_attrib:
+        return float(value)
+    elif attrib is "DateTime":
+        return convert_to_date_time(value)
+    else:
+        return value
+
+#Convierte una string a formato DateTime
+#return el valor convertido
+def convert_to_date_time(value):
+    value =  datetime.strptime('Jun 1 2005  1:33PM', '%b %d %Y %I:%M%p')
+    return value
 
 if __name__ == "__main__":
     file_data = load_and_clean_data("resources/inc2006.xml")
     incidences = extract_data(file_data)
-    
+    path_to_db = "sqlite:///sqlalchemy_example.db"
+    if path.exists(path_to_db):
+        remove(path_to_db)
+    else:
+        create_db(path_to_db)
+
+    db = get_db_session(path_to_db)
+
+    insert_incidences_into_db(incidences, db)
+
---
+++ models.py
@@ -0,0 +1,33 @@
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import session
+from sqlalchemy import Column, Integer, String, DateTime, Float, create_engine
+from datetime import datetime
+
+Base = declarative_base()
+
+class Incidencia(Base):
+    __tablename__ = "incidencia"
+
+    id = Column(Integer, primary_key=True)
+    tipo = Column(String, default="unknown")
+    autonomia = Column(String, default="unknown")
+    matricula = Column(String, default="unknown")
+    causa = Column(String, default="unknown")
+    fecha_hora_ini = Column(DateTime, default=datetime.now())
+    nivel = Column(String, default="unknown")
+    carretera = Column(String, default="unknown")
+    pk_inicial = Column(Float, default=1)
+    pk_final = Column(Float, default=1)
+    sentido = Column(String, default=1)
+    longitud = Column(Float, default=1.0)
+    latitud = Column(Float, default=1.0)
+
+def create_db(path_to_db):
+    engine = create_engine(path_to_db)
+    Base.metadata.create_all(engine)
+
+def get_db_session(path_to_db):
+    engine = create_engine(path_to_db)
+    Base.metadata.bind = engine
+    DBSession = session.sessionmaker(bind=engine)
+    return DBSession()
