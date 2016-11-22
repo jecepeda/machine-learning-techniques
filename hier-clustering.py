c0d6e0d8e66b78f4c01a6476aaa634b236620e6c
The Raez <pabloraez.1995@gmail.com>
2016-11-22 17:13:34 +0100

    there it goes the hierarchical clustering

---
+++ hier-clustering.py
@@ -0,0 +1,31 @@
+import sqlite3
+import matplotlib.pyplot as plt
+import numpy as np
+from scipy import cluster
+from sklearn import preprocessing
+import sklearn.neighbors
+
+def retrieve_data(db='incidences.db'):
+    conn = sqlite3.connect(db)
+    query_result = conn.execute("SELECT *  FROM ClustersN;")
+    ret= [row for row in query_result]
+    return ret
+
+data = retrieve_data()
+
+states = retrieve_data()
+
+min_max_scaler = preprocessing.MinMaxScaler()
+states = min_max_scaler.fit_transform(states)
+
+dist = sklearn.neighbors.DistanceMetric.get_metric('euclidean')
+matsim = dist.pairwise(states)
+avSim = np.average(matsim)
+
+print ("%s\t%6.2f" % ('Distancia Media', avSim))
+
+clusters = cluster.hierarchy.linkage(matsim, method = 'ward')
+cluster.hierarchy.dendrogram(clusters, color_threshold=0)
+plt.show()
+
+
